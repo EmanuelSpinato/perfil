c675e0598415f965b13c034277da4dfdebda74b8
EmanuelSpinato <108852799+EmanuelSpinato@users.noreply.github.com>
2024-04-26 17:17:42 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ProfileApp());
+}
+
+class ProfileApp extends StatelessWidget {
+  const ProfileApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meu Perfil',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const ProfileScreen(),
+    );
+  }
+}
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meu Perfil'),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ProfileInfo(title: 'Nome', value: 'Emanuel'),
+            ProfileInfo(title: 'Endereço', value: 'Sao francisco'),
+            ProfileInfo(title: 'Telefone', value: '+55 (46)999182884'),
+            ProfileInfo(title: 'E-mail', value: 'emanuelspinato2003@gmail.com'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileInfo extends StatelessWidget {
+  final String title;
+  final String value;
+
+  const ProfileInfo({
+    super.key,
+    required this.title,
+    required this.value,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            value,
+            style: const TextStyle(
+              fontSize: 18,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
